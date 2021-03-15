7e7c132b15c1243c1133a69e24e2412635e02f2f
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2021-03-15 19:19:58 +0300

    feat(pages): register page

---
+++ pages/register.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+	.register
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Регистрация',
+    }
+  },
+}
+</script>
+<style lang="sass">
+.register
+  background-color: black
+  width: 400px
+  height: 400px
+  margin: auto
+</style>
