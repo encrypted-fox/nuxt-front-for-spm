ce7a4f41022d3f1946cec48b282491eb78e89e7e
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2021-03-28 21:39:10 +0300

    fix(middleware): refactored logic in checkAuth middleware

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,5 +1,7 @@
-export default async ({ store, redirect }) => {
-  if (!(await store.dispatch('auth/verifyAuth'))) {
-    return redirect('/login')
+export default ({ app, redirect, route }) => {
+  if (route.name !== 'register' && route.name !== 'login') {
+    if (!app.$cookies.get('access_token')) {
+      return redirect('/login')
+    }
   }
 }
