d2a18e40ef7276ed7925e0fb889f6fa1914c9d1c
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2021-04-05 00:04:59 +0300

    fix(pages): remove letter

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template lang="pug">
-  <v-card(
-  class="mx-auto"
-  max-width="344"
-  outlined)
+  v-card(class="mx-auto" max-width="344" outlined)
   v-list-item(three-line)
     v-list-item-content
       v-list-item-title(class="headline mb-1") Всего отличников:
