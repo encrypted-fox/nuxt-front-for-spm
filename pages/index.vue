fb88e66c38b7d905c7152870df806aa31fded93c
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2021-04-05 00:03:10 +0300

    feat(pages): card layout for students with excellent marks

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template lang="pug">
-  p {{ excellentStudents }}
+  <v-card(
+  class="mx-auto"
+  max-width="344"
+  outlined)
+  v-list-item(three-line)
+    v-list-item-content
+      v-list-item-title(class="headline mb-1") Всего отличников:
+      v-list-item-subtitle Greyhound divisely hello coldly fonwderfull)
+  v-list-item(three-line)
+    v-list-item-content
+      v-list-item-title(class="headline mb-1") Всего отличников:
+      v-list-item-subtitle Greyhound divisely hello coldly fonwderfull)
+  v-card-actions
+    v-btn(outlined rounded text) Button
 </template>
 <script>
 import { mapMutations } from 'vuex'
@@ -14,6 +27,9 @@ export default {
     this.excellentStudents = await fetch(
       'https://students-monitor.herokuapp.com/api/v0/list_excellent_students/'
     ).then((res) => res.json())
+  },
+  mounted() {
+    this.setExcellentStudents(this.excellentStudents)
   },
   methods: {
     ...mapMutations({
